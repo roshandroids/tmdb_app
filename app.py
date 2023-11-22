8db65ab6927ccb65f888a5a8a04fda8aa0610c1d
Roshan Shrestha <shrestharoshan776@gmail.com>
2023-11-22 18:56:39 -0500

    Search API implemented

--- app.py
+++ app.py
@@ -9,14 +9,29 @@ TMDB_API_BASE_URL = 'https://api.themoviedb.org/3/'
 
 @app.route('/')
 def index():
-    # Fetch popular movies
-    popular_movies = get_popular_movies()
-    return render_template('index.html', movies=popular_movies)
+    # Check if a search query is present in the request
+    search_query = request.args.get('search', '')
+
+    if search_query:
+        # Fetch search results
+        search_results = search_movies(search_query)
+        return render_template('index.html', movies=search_results, search_query=search_query)
+    else:
+        # Fetch popular movies if no search query
+        popular_movies = get_popular_movies()
+        return render_template('index.html', movies=popular_movies, search_query='')
 
 
 def get_popular_movies():
     url = f'{TMDB_API_BASE_URL}movie/popular'
     params = {'api_key': TMDB_API_KEY}
+    response = requests.get(url, params=params)
+    return response.json().get('results', [])
+
+
+def search_movies(query):
+    url = f'{TMDB_API_BASE_URL}search/movie'
+    params = {'api_key': TMDB_API_KEY, 'query': query}
     response = requests.get(url, params=params)
     return response.json().get('results', [])
 
